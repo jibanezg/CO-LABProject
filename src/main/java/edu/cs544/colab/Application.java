5ffa26b9bb5639c7d667d98c8ff854b4d08f7a29
Jhonathan Ibáñez Guevara <grimgrothse@hotmail.com>
2017-12-15 16:06:25 -0600

    Deleted unnecesary datasource beans, not multiple datasources needed

--- src/main/java/edu/cs544/colab/Application.java
+++ src/main/java/edu/cs544/colab/Application.java
@@ -4,18 +4,8 @@ import edu.cs544.colab.client.Client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 
 /**
@@ -29,29 +19,6 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    @ConfigurationProperties("app.datasource")
-    public DataSource getDataSource() {
-        return dataSourceProperties().initializeDataSourceBuilder().type(DriverManagerDataSource.class).build();
-    }
 
-    @Bean
-    @Primary
-    @ConfigurationProperties("app.datasource")
-    public DataSourceProperties dataSourceProperties() {
-        return new DataSourceProperties();
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(EntityManagerFactoryBuilder builder) {
-
-        Map<String, String> properties = new HashMap<>();
-        properties.put("hibernate.hbm2ddl.auto", "create");
-
-        return builder.dataSource(getDataSource())
-                .properties(properties)
-                .packages(Client.class).persistenceUnit("COLABProject").build();
-
-    }
 
 }
