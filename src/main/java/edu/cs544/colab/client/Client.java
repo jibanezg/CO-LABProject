9f4411288cfebc073abfae84ea8aa528e14e9365
abdalla-pc <abdalla-pc@abdalla.cs.mum.edu>
2017-12-15 17:28:24 -0600

    client Entity

--- src/main/java/edu/cs544/colab/client/Client.java
+++ src/main/java/edu/cs544/colab/client/Client.java
@@ -1,5 +1,11 @@
 package edu.cs544.colab.client;
 
-public class Client {
+import javax.persistence.Entity;
+
+import edu.cs544.colab.rental.BaseEntity;
+@Entity
+public class Client extends BaseEntity{
+	
+	private static final long serialVersionUID = 1L;
 
 }
