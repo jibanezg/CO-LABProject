4cee44f993f2253a2d23725ac7e67f5e3c1f6e58
Stewart Dickson <sdickson@wolfram.com>
2017-12-15 15:35:17 -0600

    Commit before pull of abdalla-pc’s branch

--- src/main/java/edu/cs544/colab/service/IClientService.java
+++ src/main/java/edu/cs544/colab/service/IClientService.java
@@ -1,5 +1,13 @@
 package edu.cs544.colab.service;
 
-public interface IClientService {
+import java.util.Collection;
 
+import edu.cs544.colab.client.Client;
+
+public interface IClientService {
+	public void addClient(Client client);
+	public void deleteClient(String clientId);
+	public void updateClient(Client client);
+	public Collection<Client> getClients();
+	public Client getClient(String clientId);
 }
