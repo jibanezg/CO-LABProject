9da1121c682e3c22d851c80ca8249a4f931ef5b6
Jhonathan Ibáñez Guevara <grimgrothse@hotmail.com>
2017-12-15 16:39:50 -0600

    Transaction management enabled, necessary if @Transactional is to be used with Spring Data

--- src/main/java/edu/cs544/colab/Application.java
+++ src/main/java/edu/cs544/colab/Application.java
@@ -5,7 +5,7 @@ import edu.cs544.colab.client.Client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 /**
@@ -13,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @SpringBootApplication(scanBasePackages ={ "edu.cs544.colab.*"})
 @EnableJpaRepositories
+@EnableTransactionManagement
 public class Application {
 
     public static void main(String[] args) {
