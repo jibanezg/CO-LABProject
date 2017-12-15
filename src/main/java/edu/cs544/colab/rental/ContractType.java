d39f82ed2ee865b8719daaeffe8f7a64524c4064
abdalla-pc <abdalla-pc@abdalla.cs.mum.edu>
2017-12-15 15:16:27 -0600

    rental module

---
+++ src/main/java/edu/cs544/colab/rental/ContractType.java
@@ -0,0 +1,7 @@
+package edu.cs544.colab.rental;
+
+public enum ContractType {
+	
+DAILY,MONTHLY,YEARLY
+	
+}
---
+++ src/main/java/edu/cs544/colab/rental/Rental.java
@@ -0,0 +1,36 @@
+package edu.cs544.colab.rental;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import edu.cs544.colab.client.Client;
+
+@Entity
+public class Rental extends BaseEntity{
+	private static final long serialVersionUID = 1L;
+	private Date rentFrom;
+	private Date rentTo; 
+	@OneToOne
+	private Client client;
+	
+	
+	public Date getRentFrom() {
+		return rentFrom;
+	}
+	public void setRentFrom(Date rentFrom) {
+		this.rentFrom = rentFrom;
+	}
+	public Date getRentTo() {
+		return rentTo;
+	}
+	public void setRentTo(Date rentTo) {
+		this.rentTo = rentTo;
+	}
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	
+}
