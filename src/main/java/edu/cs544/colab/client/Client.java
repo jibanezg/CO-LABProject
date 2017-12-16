d26cf8bfc6a7d57b28a72b836c1f302c5951f94c
abdalla-pc <abdalla-pc@abdalla.cs.mum.edu>
2017-12-16 15:24:33 -0600

    merge with StewartSaturday

--- src/main/java/edu/cs544/colab/client/Client.java
+++ src/main/java/edu/cs544/colab/client/Client.java
@@ -1,11 +1,38 @@
 package edu.cs544.colab.client;
 
-import javax.persistence.Entity;
+import java.util.List;
 
 import edu.cs544.colab.rental.BaseEntity;
-@Entity
-public class Client extends BaseEntity{
+import edu.cs544.colab.rental.domain.Rental;
+
+public class Client extends BaseEntity {
 	
 	private static final long serialVersionUID = 1L;
+	String name;
+	String address;
+	List<Rental> rentals;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public List<Rental> getRentals() {
+		return rentals;
+	}
+	public void setRentals(List<Rental> rentals) {
+		this.rentals = rentals;
+	}
+	
+	public void addRental(Rental rental) {
+		this.rentals.add(rental);
+	}
 
 }
