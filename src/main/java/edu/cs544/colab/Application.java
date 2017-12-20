b16c021a116952f81ecb8c1fbdd46d8f11a18b36
Jhonathan Ibáñez Guevara <grimgrothse@hotmail.com>
2017-12-19 18:51:19 -0600

    Security added

--- src/main/java/edu/cs544/colab/Application.java
+++ src/main/java/edu/cs544/colab/Application.java
@@ -1,9 +1,13 @@
 package edu.cs544.colab;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -19,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @EnableJpaRepositories
 @EnableTransactionManagement
 @Controller
-public class Application {
+public class Application extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -28,6 +32,27 @@ public class Application {
     @RequestMapping(value = {"/","/index"})
     public String home(){
         return "redirect:/ListOffice";
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
     }
 
 }
--- src/main/java/edu/cs544/colab/office/controller/OfficeController.java
+++ src/main/java/edu/cs544/colab/office/controller/OfficeController.java
@@ -87,4 +87,5 @@ public class OfficeController {
         return "officeSuccess";
     }
 
+
 }
